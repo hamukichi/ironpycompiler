7a840983dd013932fc08dd74cd61dfbb9115314f
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-03-11 20:57:47 +0900

    Revised setup.py

--- ironpycompiler/ipy2asm.py
+++ ironpycompiler/ipy2asm.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # Original modules
-from . import compiler
+import ironpycompiler.compiler as compiler
 
 def _compiler(args):
     """Funciton for command ``compile``. It should not be used directly.
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ This is a script for installing IronPyCompiler.
 
 """
 
-
 from setuptools import setup
+import sys
 
 import ironpycompiler
 
@@ -15,28 +15,37 @@ import ironpycompiler
 with open("README.txt", "r") as f:
     readme_content = f.read()
 
-setup(name = "ironpycompiler",
-      version = ironpycompiler.__version__,
-      description = "Compile IronPython scripts into a stand-alone .NET assembly.", 
-      long_description = readme_content, 
-      author = "Hamukichi (Nombiri)", 
-      author_email = "hamukichi-dev@outlook.jp",
-      packages = ["ironpycompiler"], 
-      install_requires = ["argparse"], 
-      provides = ["ironpycompiler"], 
-      scripts = ["ipy2asm.py"], 
-      url = "https://github.com/hamukichi/ironpycompiler", 
-      classifiers = ["Development Status :: 3 - Alpha", 
-                     "Intended Audience :: Developers", 
-                     "License :: OSI Approved :: MIT License", 
-                     "Operating System :: Microsoft :: Windows", 
-                     "Programming Language :: Python", 
-                     "Programming Language :: Python :: 2", 
-                     "Programming Language :: Python :: Implementation :: CPython", 
-                     "Programming Language :: Python :: Implementation :: PyPy", 
-                     "Topic :: Software Development", 
-                     "Topic :: System :: Software Distribution"],
-      license = "MIT License", 
-      keywords = ["ironpython", ".net", "assembly", "executable", "compile", "stand-alone", "pyc.py"], 
-      entry_points = {"console_scripts": ["ipy2asm = ironpycompiler.ip2asm:main"]}
-      )
+sysver = sys.version_info
+
+classifiers = ["Development Status :: 3 - Alpha", 
+               "Intended Audience :: Developers", 
+               "License :: OSI Approved :: MIT License", 
+               "Operating System :: Microsoft :: Windows", 
+               "Programming Language :: Python", 
+               "Programming Language :: Python :: 2", 
+               "Programming Language :: Python :: Implementation :: CPython", 
+               "Programming Language :: Python :: Implementation :: PyPy", 
+               "Topic :: Software Development", 
+               "Topic :: System :: Software Distribution"]
+
+setup_args = {"name": "ironpycompiler",
+              "version": ironpycompiler.__version__,
+              "description": "Compile IronPython scripts into a stand-alone .NET assembly.", 
+              "long_description": readme_content, 
+              "author": "Hamukichi (Nombiri)", 
+              "author_email": "hamukichi-dev@outlook.jp",
+              "packages": ["ironpycompiler"],
+              "provides": ["ironpycompiler"],
+              "url": "https://github.com/hamukichi/ironpycompiler", 
+              "classifiers": classifiers, 
+              "license" : "MIT License", 
+              "keywords": ["ironpython", ".net", "assembly", "executable", 
+                   "compile", "stand-alone", "pyc.py"], 
+              "install_requires": [], 
+              "entry_points": {"console_scripts": 
+                        ["ipy2asm = ironpycompiler.ipy2asm:main"]}}
+
+if sysver[0] == 2 and sysver[1] < 7:
+    setup_args["install_requires"].append("argparse")
+
+setup(**setup_args)
