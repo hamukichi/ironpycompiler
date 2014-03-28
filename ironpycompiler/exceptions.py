cf691379add6a30756e0e39d9bf204fc1df309e4
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-03-28 22:35:18 +0900

    Updated exceptions.IronPythonDetectionError

--- ironpycompiler/exceptions.py
+++ ironpycompiler/exceptions.py
@@ -14,24 +14,27 @@ class IPCError(Exception):
 class IronPythonDetectionError(IPCError):
     """This exception will be raised when IronPython cannot be found in your system.
     
-    :param str executable: (optional) The name of the IronPython executable looked for.
+    :param str executable: (optional) The name of the IronPython 
+                           executable looked for. This argument remains 
+                           for backward compatibility.
+    :param msg: (optional) The detailed information of the error.
     
     .. versionchanged:: 0.9.0
-       The argument ``executable`` became optional.
+       The argument ``executable`` became optional, and the argument ``msg`` was added.
     
     """
     
-    def __init__(self, executable = None):
-        if executable is None:
-            self.executable = None
-        else:
-            self.executable = str(executable)
+    def __init__(self, executable = None, msg = None):
+        self.executable = executable
+        self.msg = msg
     
     def __str__(self):
-        if self.executable is None:
-            return "IronPython cannot be found."
+        if self.executable is not None:
+            return "IronPython (%s) cannot be found." % str(self.executable)
+        elif self.msg is not None:
+            return str(self.msg)
         else:
-            return "IronPython (%s) cannot be found." % self.executable
+            return "IronPython cannot be found."
 
 
         
