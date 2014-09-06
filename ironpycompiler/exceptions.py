710affa1c4d93cc629048b32c3707c3a3980a6ce
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-09-06 18:30:52 +0900

    Revised accoding to PEP8
    E302, W293, E501, W291, E251, W391, ...

--- ironpycompiler/exceptions.py
+++ ironpycompiler/exceptions.py
@@ -5,29 +5,33 @@
 
 """
 
+
 class IPCError(Exception):
+
     """This is the base class for exceptions in this module.
-    
+
     """
     pass
 
+
 class IronPythonDetectionError(IPCError):
-    """This exception will be raised when IronPython cannot be found in your system.
-    
-    :param str executable: (optional) The name of the IronPython 
-                           executable looked for. This argument remains 
+
+    """This exception will be raised if IronPython cannot be found in your system.
+
+    :param str executable: (optional) The name of the IronPython
+                           executable looked for. This argument remains
                            for backward compatibility.
     :param msg: (optional) The detailed information of the error.
-    
+
     .. versionchanged:: 0.9.0
-       The argument ``executable`` became optional, and the argument ``msg`` was added.
-    
+       The argument ``executable`` became optional, and ``msg`` was added.
+
     """
-    
-    def __init__(self, executable = None, msg = None):
+
+    def __init__(self, executable=None, msg=None):
         self.executable = executable
         self.msg = msg
-    
+
     def __str__(self):
         if self.executable is not None:
             return "IronPython (%s) cannot be found." % str(self.executable)
@@ -36,21 +40,22 @@ class IronPythonDetectionError(IPCError):
         else:
             return "IronPython cannot be found."
 
+
 class ModuleCompilationError(IPCError):
+
     """This exception means an error during compilation.
-    
+
     :param msg: (optional) The detailed information of the error.
-    
+
     .. versionadded:: 0.10.0
-    
+
     """
-    
-    def __init__(self, msg = None):
+
+    def __init__(self, msg=None):
         self.msg = msg
-    
+
     def __str__(self):
         if self.msg is not None:
             return str(self.msg)
         else:
             return "An error occurred during compilation."
-
