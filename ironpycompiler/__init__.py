3b3e89341bd2fc26852741e3fd312126c956a108
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-09-06 18:24:14 +0900

    Revised according to PEP8
    Removed trailing whitespace (W291)
    Removed the blank line at EOF (W391)

--- ironpycompiler/__init__.py
+++ ironpycompiler/__init__.py
@@ -3,12 +3,12 @@
 
 """Helps to compile IronPython scripts, using pyc.py.
 
-This library helps you compile your IronPython scripts requiring the 
-Python standard library (or third-party pure-Python modules) into a 
+This library helps you compile your IronPython scripts requiring the
+Python standard library (or third-party pure-Python modules) into a
 .NET assembly, using pyc.py.
 
-.. note :: This library should be used on **CPython**, not IronPython, 
-           because :mod:`modulefinder` of IronPython does not work 
+.. note :: This library should be used on **CPython**, not IronPython,
+           because :mod:`modulefinder` of IronPython does not work
            correctly.
 """
 
@@ -16,4 +16,3 @@ __author__ = "Hamukichi (Nombiri)"
 __version__ = "0.10.1"
 __date__ = "2014-08-30"
 __licence__ = "MIT License"
-
