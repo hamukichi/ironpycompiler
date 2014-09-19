fee7821e3864b63b18d1e5cdb291fa353b238b9a
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-09-20 00:36:04 +0900

    Updated PythonVersion
    * Added docstring
    * Corrected __init__
    * Formatted using (auto)pep8 and pyflakes

--- ironpycompiler/datatypes.py
+++ ironpycompiler/datatypes.py
@@ -7,15 +7,27 @@
 
 import distutils.version
 
+
 class PythonVersion(distutils.version.StrictVersion):
 
     """Represents a Python version.
 
+    :param str vstring: String showing a Python version, like '2.7.8'.
+
+    .. versionadded:: 1.0.0
     """
 
     def __init__(self, vstring=None):
-        distutils.version.StrictVersion.__init__(self)
-        self.major = self.version[0]
-        self.minor = self.version[1]
-        self.patch = self.version[2]
+        """Initalize the instance.
 
+        """
+        distutils.version.StrictVersion.__init__(self, vstring)
+
+        #: Integer showing the major version.
+        self.major = self.version[0]
+
+        #: Integer showing the minor version.
+        self.minor = self.version[1]
+
+        #: Integer showing the patch version.
+        self.patch = self.version[2]
