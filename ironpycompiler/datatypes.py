1ebc957d194bb2b0d92064524de48f12391f44c2
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-09-20 00:21:02 +0900

    Added PythonVersion

--- ironpycompiler/datatypes.py
+++ ironpycompiler/datatypes.py
@@ -5,3 +5,17 @@
 
 """
 
+import distutils.version
+
+class PythonVersion(distutils.version.StrictVersion):
+
+    """Represents a Python version.
+
+    """
+
+    def __init__(self, vstring=None):
+        distutils.version.StrictVersion.__init__(self)
+        self.major = self.version[0]
+        self.minor = self.version[1]
+        self.patch = self.version[2]
+
