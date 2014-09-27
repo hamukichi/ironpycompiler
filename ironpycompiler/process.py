64d0a44b6749f13cf7b2075663f94116d4cf1c6b
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2014-09-27 18:21:48 +0900

    Now execute_ipy handles cwd

--- ironpycompiler/process.py
+++ ironpycompiler/process.py
@@ -9,12 +9,14 @@ import subprocess
 import os
 
 
-def execute_ipy(path_to_exe, arguments):
+def execute_ipy(path_to_exe, arguments, cwd=None):
     """Executes the IronPython executable with the provided arguments.
 
     :param str path_to_exe: The path to the IronPython executable.
     :param list arguments: The arguments that should be passed to the
                            IronPython executable.
+    :param str cwd: Specify the working directory, or :func:`os.getcwd` will
+                    be used.
     :return: A tuple containing a string showing stdout/stderr, and the
              return code
     :rtype: tuple
@@ -35,6 +37,7 @@ def execute_ipy(path_to_exe, arguments):
     ipy_sp = subprocess.Popen(
         args=[os.path.basename(path_to_exe)] + arguments,
         executable=path_to_exe, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT, universal_newlines=True)
+        stderr=subprocess.STDOUT, universal_newlines=True,
+        cwd=(cwd if cwd is not None else os.getcwd()))
     output = ipy_sp.communicate()
     return (output[0], ipy_sp.returncode)
